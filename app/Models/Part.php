099dc182a7a17e603f036881262d292740aa457f
s.sagawa <s.sagawa@suday-ja.com>
2022-02-01 15:28:18 +0900

    モデルリレーション

--- app/Models/Part.php
+++ app/Models/Part.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Part extends Model
 {
     use HasFactory;
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_part');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function parts()
+    {
+        return $this->belongsToMany(Part::class, 'product_part');
+    }
 }
