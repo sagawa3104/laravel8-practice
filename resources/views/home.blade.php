be6353edeb13092be67dec01a561658168634043
s.sagawa <s.sagawa@suday-ja.com>
2022-02-02 02:52:06 +0900

    ログイン後画面表示

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html lang="ja">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="./css/destyle.css" media="all">
+        <link rel="stylesheet" type="text/css" href="./css/app.css" media="all">
+    </head>
+    <body>
+        <h1>Home</h1>
+    </body>
+</html>
