70d1f65359ac4158180a3dcb466e78216ff63f82
s.sagawa <s.sagawa@suday-ja.com>
2022-02-01 15:06:46 +0900

    工程モデルリソース関連

---
+++ app/Http/Controllers/ProcessController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProcessRequest;
+use App\Http\Requests\UpdateProcessRequest;
+use App\Models\Process;
+
+class ProcessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProcessRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProcessRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Process  $process
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Process $process)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Process  $process
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Process $process)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProcessRequest  $request
+     * @param  \App\Models\Process  $process
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProcessRequest $request, Process $process)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Process  $process
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Process $process)
+    {
+        //
+    }
+}
