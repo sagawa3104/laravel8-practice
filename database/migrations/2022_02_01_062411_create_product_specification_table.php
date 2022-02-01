4ef569f33f531f70816d3a452dd7fe71346b8a29
s.sagawa <s.sagawa@suday-ja.com>
2022-02-01 23:47:47 +0900

    品目-仕様中間テーブル

---
+++ database/migrations/2022_02_01_062411_create_product_specification_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSpecificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_specification', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('specification_id');
+            $table->timestamps();
+
+            // ユニーク設定
+            $table->unique([
+                'product_id',
+                'specification_id'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_specification');
+    }
+}
