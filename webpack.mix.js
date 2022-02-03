9e5d50ab8c0ba72d30d3ba75a676f0fb61d45c8f
s.sagawa <s.sagawa@suday-ja.com>
2022-02-03 22:16:42 +0900

    フロントビルドスクリプト更新

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
     .postCss('resources/css/destyle.css', 'public/css', [
         //
     ]);
+
+mix.sass('resources/sass/app.scss', 'public/css');
