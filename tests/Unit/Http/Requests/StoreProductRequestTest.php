137fb95e47a5b0b1e472c619538e9a9ff327dc16
s.sagawa <s.sagawa@suday-ja.com>
2022-02-04 09:59:45 +0900

    正常データのテスト

--- tests/Unit/Http/Requests/StoreProductRequestTest.php
+++ tests/Unit/Http/Requests/StoreProductRequestTest.php
@@ -105,4 +105,23 @@ test('品目名称の最大文字列長違反', function () {
 
 });
 
+test('品目コード・名称の正常データ', function () {
+
+    // Arrange
+    $data = [
+        'product_code' => str_repeat('a', 32),
+        'product_name' => str_repeat('a', 255),
+    ];
+    $request = new StoreProductRequest();
+    $rules = $request->rules();
+    $validator = Validator::make($data, $rules);
+
+    // Act
+    $isFail = $validator->fails();
+
+    // Assert
+    expect($isFail)->toBeFalse();
+
+});
+
 
