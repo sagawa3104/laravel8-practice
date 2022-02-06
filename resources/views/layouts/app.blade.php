d5b21860060d1df7bccaedb7e17439eb0829e3cf
Satoshi <sagawasatoshi3104@gmail.com>
2022-02-06 10:01:21 +0900

    アセットURL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="./css/destyle.css" media="all">
-        <link rel="stylesheet" type="text/css" href="./css/app.css" media="all">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/destyle.css')}}" media="all">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}" media="all">
     </head>
     <body>
         <title>ログイン</title>
