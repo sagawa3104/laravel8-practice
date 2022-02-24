a1aea341b7556289844e0f0a0aab4f2677efde36
s.sagawa <s.sagawa@suday-ja.com>
2022-02-25 01:52:02 +0900

    修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,39 +8,39 @@
         <p>ログイン後ページ</p>
         <br>
         <p>メモ</p>
-        <ul class="vertical-list">
+        <ul class="vertical-list vertical-list--indent">
             <li>品目
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>品目は複数の部位を持つ</li>
                     <li>品目は複数の仕様を持つ</li>
                 </ul>
             </li>
             <li>部位
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>部位は複数の品目に適用される</li>
                     <li>部位は一枚の図面を持つ</li>
                 </ul>
             </li>
             <li>仕様
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>仕様は複数の品目に適用される</li>
                     <li>部位は一枚の図面を持つ</li>
                 </ul>
             </li>
             <li>検査方式
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>チェックリスト方式とマッピング方式がある</li>
                     <li>検査方式は品目・工程によって決まる</li>
                 </ul>
             </li>
             <li>検査項目
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>チェックリスト方式の場合、品目の仕様および製番ごとの専用仕様から構成される</li>
                     <li>マッピング方式の場合、工程・部位ごとの検査項目から構成される</li>
                 </ul>
             </li>
             <li>検査
-                <ul class="vertical-list">
+                <ul class="vertical-list vertical-list--indent">
                     <li>製造番号・工程ごとに実施される</li>
                     <li>検査結果として検査項目を複数持つ</li>
                 </ul>
